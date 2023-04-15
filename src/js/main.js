b963af10d242983fcd9e1fb4de0855125f89253d
Neyzer <est.njbelloring0201@uml.edu.ni>
2023-04-15 12:37:55 -0600

    creacion de pagina y detalles

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,12 @@ import '/src/css/style.css';
 
 //activar el plugin del flowbite
 import 'flowbite';
+
+//cargar la barra de navegacion 
+import './barra'
+
+//cargar el menu del carusel/slider
+import './slider'
+
+//importar el header
+import './header'
